5479979211182b0f88328123a7981a8dfe852ae1
Ameer Hamza <ah930031@gmail.com>
2024-08-13 19:40:12 +0500

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,54 @@
+<style>
+	.float-button {
+        text-decoration: none;
+        position: fixed;
+        padding: 10px;
+        padding-right: 15px;
+        bottom: 30px;
+        right: 30px;
+        color: #fff;
+        background-color:#25D366;
+        border-radius: 25px 30px 5px 25px;
+        z-index: 100;
+        font-family: Arial;
+        font-family: Arial;
+        font-size: 17px;
+        animation: whatsapp-animation 0.5s ease-in-out;
+        box-shadow: 1px 2px 5px 2px rgba(30,30,30,0.3);
+        transition:all 0.3s ease-out;
+    }
+
+    .float-button:hover {
+         background-color: #128C7E;
+          color: #fff;
+    }
+
+   
+
+    @media screen and (max-width: 545px) {
+        span {
+            display: none;
+        }
+
+        .float-button {
+            bottom: 15px;
+            right: 15px;
+            width: 20px;
+            border-radius: 20px 20px 5px 20px;
+        }
+
+        
+</style>
+
+<script src="assets/js/jquery-1.11.1.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/jquery.dataTables.min.js"></script>
+<script src="assets/js/dataTables.bootstrap.min.js"></script>
+<script src="assets/js/foundation-datepicker.min.js"></script>
+<script src="assets/js/validator.min.js"></script>
+<script src="assets/js/custom.js"></script>
+<script src="assets/ajax.js"></script>
+ 
+
+</body>
+</html>
